28c545901b4424ea471444322332903b205b4290
keno <keno123@126.com>
2020-08-17 20:38:07 +0800

    add README.md

--- app/src/main/java/com/kiscode/java8/MainActivity.java
+++ app/src/main/java/com/kiscode/java8/MainActivity.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,15 +24,32 @@ public class MainActivity extends AppCompatActivity {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                Log.i("Runnable","Runnable");
+                Log.i("Runnable", "Runnable");
             }
         }).start();
 
 
-        new Thread(() -> Log.i("Runnable","Runnable")).start();
+        new Thread(() -> Log.i("Runnable", "Runnable")).start();
 
 
         List<String> list = Arrays.asList("A", "B", "C", "D", "NB", "CBA", "DNF");
-        list.forEach(s -> Log.i("forEach",s));
+        list.forEach(s -> Log.i("forEach", s));
+
+        list.stream().map(new Function<String, String>() {
+            @Override
+            public String apply(String s) {
+                return "String is " + s;
+            }
+        }).forEach(new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                Log.i("string", s);
+            }
+        });
+
+        //lambda表达式实现
+        list.stream().map(s -> "String is " + s)
+                .forEach(s ->
+                        Log.i("string", s));
     }
 }
