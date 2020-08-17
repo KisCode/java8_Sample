30210267d94b3892a7fae89cb3b5d87b3e012d7d
阚健雄 <kanjianxiong@rlair.net>
2020-08-17 17:22:30 +0800

    Android Studio 4.0 for java8:无需给应用程序设置最低 API 级别

--- app/src/main/java/com/kiscode/java8/MainActivity.java
+++ app/src/main/java/com/kiscode/java8/MainActivity.java
@@ -1,14 +1,20 @@
 package com.kiscode.java8;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +29,9 @@ public class MainActivity extends AppCompatActivity {
 
 
         new Thread(() -> Log.i("Runnable","Runnable")).start();
+
+
+        List<String> list = Arrays.asList("A", "B", "C", "D", "NB", "CBA", "DNF");
+        list.forEach(s -> Log.i("forEach",s));
     }
 }
